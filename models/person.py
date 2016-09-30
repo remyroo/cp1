253c924a307f2f5f0de4a6ce1a1c07f66362851d
Rehema <rehema.wachira@andela.com>
2016-09-30 15:00:14 +0300

    Test-Add test dependancies

---
+++ models/person.py
@@ -0,0 +1,24 @@
+class Person(object):
+
+	def create_person(self, name):
+		pass
+
+	def get_number_of_people(self):
+		pass
+
+	def reallocate_person(self, name, room_name):
+		pass
+
+	def get_list_of_people_in_room(self, room_name):
+		pass
+
+	def load_people(self, filname):
+		pass
+
+	def print_room_allocations(self, filename):
+		pass
+
+	def print_unallocated_people(self, filename):
+		pass
+
+
