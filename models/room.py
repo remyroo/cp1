b82f0d1b5f29fa629df583939209d2bd5adb1e08
Rehema <rehema.wachira@andela.com>
2016-09-30 14:46:31 +0300

    ft-Add initial models

---
+++ models/room.py
@@ -0,0 +1,17 @@
+class Room(object):
+
+	def create_room(self, room_name):
+		#room_name = room_name
+		#room_type = input("gimme:")
+		pass
+
+	def get_number_of_rooms(self):
+		pass
+	
+	def assign_room(self, name, room_name):
+		pass
+
+	def is_room_full(self, room_name):
+		#if occupancy = capacity 
+		#return true
+		pass
